65acc55f8d0a0611da49002c80caf061df011f6f
Jimichelle <76010791+Jimichelle@users.noreply.github.com>
2024-03-27 20:54:42 +0100

    1. Basic Data Types

---
+++ 1. Basic Data Types/data.rb
@@ -0,0 +1,53 @@
+## Basic Data Types
+
+### Numbers
+
+# Addition
+
+1 + 1 # => 2
+
+# Subtraction
+
+2 - 1 # => 1
+
+# Multiplication
+
+2 * 2 # => 4
+
+# Division
+
+10 / 5 # => 2
+
+# Exponentiation
+
+2 ** 2 # => 4
+3 ** 4 # => 81
+
+# Modulo
+
+10 % 3 # => 1
+
+# Integer and floats
+
+17 / 5 # => 3
+
+17.0 / 5 # => 3.4
+
+# To convert an integer to a float
+
+17.to_f # => 17.0
+
+# To convert a float to an integer
+
+17.0.to_i # => 17
+13.6.to_i # => 13
+
+# Methods
+
+6.even? # => true
+7.even? # => false
+
+6.odd? # => false
+7.odd? # => true
+
+### Strings
